72aec3098d13ae65ef39a8654d0765b22f398f16
Daniel Lombraña González <teleyinex@gmail.com>
2024-03-12 14:38:26 +0100

    feat: enable several plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,11 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.dart"},
+  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim"},
+  { import = "astrocommunity.editing-support.ultimate-autopair-nvim"},
+  { import = "astrocommunity.lsp.lsp-signature-nvim"},
+  { import = "astrocommunity.scrolling.cinnamon-nvim"}
 }
